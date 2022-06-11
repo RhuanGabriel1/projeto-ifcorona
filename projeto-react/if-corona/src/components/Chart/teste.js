1fd3c3f681b8a780af727aecad572ca8879849d3
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-06-10 21:35:17 -0300

    colocando um título teste

--- projeto-react/if-corona/src/components/Chart/teste.js
+++ projeto-react/if-corona/src/components/Chart/teste.js
@@ -3,13 +3,14 @@ import React from "react";
 
 const Teste = {
 
+    title: {
+        text: 'Primeiro gráfico teste!'
+    },
+
     series:[{
         name: 'Profit',
         data:[100, 200, 300, 400, 50, 10,25]
-    }],
-    title :{
-        Text:'A'
-    }
+    }]
 
 };
 
