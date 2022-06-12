1d31542afd72493474a94060e9a5528063523987
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-06-11 22:13:16 -0300

    Colocando charts para testar espaçamento
    Co-Authored-By: Marcelo13104 <92059888+Marcelo13104@users.noreply.github.com>

--- projeto-react/if-corona/src/components/ChartSpaces/ChartSpaces.js
+++ projeto-react/if-corona/src/components/ChartSpaces/ChartSpaces.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../ChartSpaces/ChartSpaces.css';
-import Charts from '../Charts/Charts';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+import ChartConfirmedCases from '../Charts/ChartConfirmedCases';
+import ChartConfirmedDeaths from '../Charts/ChartConfirmedDeaths';
+import ChartRecoveredCases from '../Charts/ChartRecoveredCases';
+import ChartStateMap from '../Charts/ChartStateMap';
+
 
 const Chart = (props) =>{
 
@@ -12,24 +14,25 @@ const Chart = (props) =>{
 
             <div className="first-inner-container">
                 <div className="first-left-space">
-                    <h1 className="vertical"><span>Primeiro bloco</span></h1>
+                    <h1 className="vertical"><span>Casos Confirmados</span></h1>
+                    <ChartConfirmedCases/>
                 </div>
 
                 <div className="first-right-space">
-                    <h1 className="vertical"><span>Segundo bloco</span></h1>
-
+                    <h1 className="vertical"><span>Óbitos Confirmados</span></h1>
+                    <ChartConfirmedDeaths/>
                 </div>
             </div>
 
             <div className="second-inner-container">
                 <div className="second-left-space">
-                    <h1 className="vertical"><span>Terceiro bloco</span></h1>
-
+                    <h1 className="vertical"><span>Casos Recuperados</span></h1>
+                    <ChartRecoveredCases/>
                 </div>
 
                 <div className="second-right-space">
-                    <h1 className="vertical"><span>Quarto bloco</span></h1>
-
+                    <h1 className="vertical"><span>Mapa do Estado de São Paulo</span></h1>
+                    <ChartStateMap/>
                 </div>
             </div>
 
---
+++ projeto-react/if-corona/src/components/Charts/ChartStateMap.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../Charts/Charts.css'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    chart: {
+        backgroundColor: '#FCFFC5',
+        type: 'line'
+    },
+
+    title:{
+        text: "Casos Confirmados de Covid-19"
+    },
+    series:[
+        {
+        name: "Casos Confirmados",
+        data: [1000, 1500, 2000, 2500 ]
+        },
+
+        {
+            name: "Teste",
+            data: [100, 150, 200, 250 ]
+    }],
+
+}
+
+const ChartStateMap = (props) =>{
+    return(
+        <>
+            <HighchartsReact highcharts={Highcharts} options={options}/>
+        </>
+    )
+}
+
+
+export default ChartStateMap;
